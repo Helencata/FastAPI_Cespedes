0ccf8b29eb9719300006623ef1c99be5d708007b
Helen <2024_helen.cespedes@iticbcn.cat>
2025-10-18 17:14:20 +0200

    commit del sabado, pero no acabado aun

--- main.py
+++ main.py
@@ -7,40 +7,39 @@ list_users = ["Helen", "Carmen", "Cristian"]
 #exercici 1. Afegir usuari
 @app.post("/api/users/{id}", response_model = dict) #el {id} se pone para poder utilizar luego, no forma parte de endpoint/ruta y el response model lo tenemos que poner siempre
 async def add_user(id): #aqui le pasamos por parametro el id de arriba
-    list_users.append(id) #aqui lo añadimos a la lista
-    dic =dict(zip(list_users, range(len(list_users)))) #aqui lo transformamos en diccionario
-    return dic #aqui retornamos el diccionario
+    list_users.append(id) #aqui añadimos un neuvo id a la lista
+    dic = {} #creamos un diccionario vacio
+    for i in range(len(list_users)): #bucle para recorrer cada elemento de la lista
+        dic[i] = list_users[i] #crea una clave para cada elemeto de la lista
+    return dic #retorna en lista
 
 #exercici 2. Llegir- Consultar un usuari
-@app.get("/api/users/{id}", response_model =dict)
-async def get_user(id):
+@app.get("/api/users/{id}", response_model = dict)
+async def get_user(id: int):
+    dic = {}
+    for i in range(len(list_users)):
+        if i == id: #verificamos si el id pasado por parametro es igual que el de la lista
+            dic[i] = list_users[i]
+            return dic
+
+#exercici 3. Llegir - Consultar tots els usuaris
+@app.get("/api/users", response_model = dict)
+async def get_users():
+    dic = {}
+    for i in range(len(list_users)):
+        dic[i] = list_users[i]
+    return dic
+
+#exercici 4. Actualitzar - Actualització completa
 
 
 
 
-
-
-@app.get("/prova/user/{user_id}")
-async def read_user(user_id:int):
-    return {"result": list_users[user_id-1]}
-
-@app.post("/prova/pro")
-async def prova_tres():
-    return {"msg":"Hello pro"}
-
-@app.delete("/prova/{user_id}")
-async def del_user():
-    return {"result": list_users}
-#pueden ser mismas rutas en diferentes metodos
-#el nombre de las funciones tienen que ser diferentes
-
-#clase roger 15/10
-
-@app.post("/hola/{id}", response_model = dict) #esto es que si o si tiene que retornar un diccionario, con eso te asegura
-def addusers(id):
-
-    #añadir nuevo usuario a list
-    list.append(id)
-    #convertir la lista en dict
-    dict = list_users
-    return dict
+#exercici 6. Eliminar - Esborrar usuari
+@app.delete("/api/usuaris/{id}", response_model = dict)
+async def delete_user(id: int):
+    list_users.pop(id) #esto borra el id pasado por parametro
+    dic = {}
+    for i in range(len(list_users)):
+        dic[i] = list_users[i]
+    return dic
